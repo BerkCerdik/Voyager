98be6ccd14ba22cbe283555334854173f46fcccd
Yunus Pazarlı <yunuspazarli@gmail.com>
2020-12-03 15:44:08 +0300

    Controller Update

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Voyager.Models.Orm.Context;
+using Voyager.Models.Orm.Entities;
+
+namespace Voyager.Controllers
+{
+    public class DriverController : Controller
+    {
+
+        private readonly VoyagerContext _context;
+        public DriverController(VoyagerContext context)
+        {
+            _context = context;
+        }
+
+    
+        public IActionResult Index()
+        {
+            List<Driver> drivers = _context.Drivers.ToList();
+            return View(drivers);
+        }
+
+    }
+}
---
+++ Controllers/PassengerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Voyager.Models.Orm.Context;
+using Voyager.Models.Orm.Entities;
+
+namespace Voyager.Controllers
+{
+    public class PassengerController : Controller
+    {
+        private readonly VoyagerContext _context;
+
+        public PassengerController(VoyagerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Passenger> passengers = _context.Passengers.ToList();
+            return View(passengers);
+        }
+        
+    }
+}
