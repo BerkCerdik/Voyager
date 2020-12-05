ce4af358e942ae62ac97822686e1590b8490cc67
Yunus Pazarlı <yunuspazarli@gmail.com>
2020-12-05 15:20:07 +0300

    Added controllers and viewpages, linked pages

--- Models/Vm/PassengerVM.cs
+++ Models/Vm/PassengerVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,22 @@ namespace Voyager.Models.Vm
 {
     public class PassengerVM
     {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please fill required areas!")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please fill required areas!")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please fill required areas!")]
         public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Please fill required areas!")]
         public string Password { get; set; }
+
+
+
+        
     }
 }
