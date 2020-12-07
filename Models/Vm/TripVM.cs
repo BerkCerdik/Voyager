37ef83c4135f45e9bd998daa1939fa0e4cf5f515
BerkCerdik <berkcerdik@gmail.com>
2020-12-07 15:28:43 +0300

    Trip ViewModel added

--- Models/Vm/TripVM.cs
+++ Models/Vm/TripVM.cs
@@ -7,5 +7,11 @@ namespace Voyager.Models.Vm
 {
     public class TripVM
     {
+        public int ID { get; set; }
+        public int PassengerID { get; set; }
+        public int DriverID { get; set; }
+        public double Price{ get; set; }
+        public string DeparturePoint { get; set; }
+        public string ArrivalPoint { get; set; }
     }
 }
