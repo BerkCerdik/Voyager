e61012e834015f43b0190e6081311fa3d4d8aec8
Yunus Pazarlı <yunuspazarli@gmail.com>
2020-12-08 00:01:32 +0300

    Trip Page Updated, Add Driver Button added

--- Controllers/AdminTripController.cs
+++ Controllers/AdminTripController.cs
@@ -3,14 +3,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Voyager.Models.Orm.Context;
+using Voyager.Models.Vm;
+using Voyager.Models.Orm.Entities;
 
 namespace Voyager.Controllers
 {
     public class AdminTripController : Controller
     {
+        private readonly VoyagerContext _context;
+        public AdminTripController(VoyagerContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<TripVM> trips = _context.Trips.Select(q => new TripVM()
+            {
+                ID = q.ID,
+                PassengerID = q.PassengerID,
+                DriverID = q.DriverID,
+                DeparturePoint = q.DeparturePoint,
+                ArrivalPoint = q.ArrivalPoint,
+               //Price eklenecek
+
+            }).ToList();
+
+            return View(trips);
+            
         }
     }
 }
