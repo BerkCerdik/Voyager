6f7db2560cd3bf7d18cacb6324ce2fe1262cc160
BerkCerdik <berkcerdik@gmail.com>
2020-12-07 16:11:59 +0300

    TripVM added with all prop. TripController modified.

--- Controllers/AdminTripController.cs
+++ Controllers/AdminTripController.cs
@@ -1,16 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Voyager.Models.Orm.Context;
+using Voyager.Models.Vm;
 
 namespace Voyager.Controllers
 {
     public class AdminTripController : Controller
     {
+        
+            private readonly VoyagerContext _context;
+
+            public AdminTripController(VoyagerContext context)
+            {
+                _context = context;
+            }
         public IActionResult Index()
         {
-            return View();
+                   
+
+            List<TripVM> trips = _context.Trips.Include(x => x.Payment).Include(a => a.Passenger).Include(b => b.Driver).Select(q => new TripVM()
+            {
+                ID = q.ID,
+                PassengerName = q.Passenger.Name,
+                PassengerLastname = q.Passenger.Surname,
+                DriverName = q.Driver.Name,
+                DriverLastname = q.Driver.Surname,
+                Price = q.Payment.Price,
+                DeparturePoint = q.DeparturePoint,
+                ArrivalPoint = q.ArrivalPoint
+
+
+            }).ToList();
+
+            return View(trips);
         }
     }
 }
--- Models/Vm/TripVM.cs
+++ Models/Vm/TripVM.cs
@@ -8,8 +8,10 @@ namespace Voyager.Models.Vm
     public class TripVM
     {
         public int ID { get; set; }
-        public int PassengerID { get; set; }
-        public int DriverID { get; set; }
+        public string PassengerName { get; set; }
+        public string PassengerLastname { get; set; }
+        public string DriverName { get; set; }
+        public string DriverLastname { get; set; }
         public double Price{ get; set; }
         public string DeparturePoint { get; set; }
         public string ArrivalPoint { get; set; }
